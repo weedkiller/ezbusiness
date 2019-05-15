5039033c216422fcfbff8ec58fc69691e26d6631
sharyu <ezwaretechano@gmail.com>
2019-05-15 17:08:38 +0400

    FFM_Com master

---
+++ EzBusiness_BL_Interface/FreightManagementBLI/IFFM_JOBFreightService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EzBusiness_DL_Interface.FreightManagementDLI;
+using EzBusiness_DL_Repository.FreightManagementDLR;
+using EzBusiness_EF_Entity.FreightManagementEF;
+using EzBusiness_ViewModels.Models.FreightManagement;
+
+namespace EzBusiness_BL_Interface.FreightManagementBLI
+{
+    public interface IFFM_JOBFreightService
+    {
+        #region FFM_JOB Master
+        List<FFM_JOB_VM> GetFFM_JOB(string CmpyCode);
+        FFM_JOB_VM SaveFFM_JOB(FFM_JOB_VM FJ);
+        bool DeleteFFM_JOB(string FFM_JOB_CODE, string CmpyCode, string UserName);
+        #endregion
+    }
+}
---
+++ EzBusiness_DL_Interface/FreightManagementDLI/IFFM_JOBRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EzBusiness_EF_Entity.FreightManagementEF;
+using EzBusiness_ViewModels.Models.FreightManagement;
+
+namespace EzBusiness_DL_Interface.FreightManagementDLI
+{
+    public interface IFFM_JOBRepository
+    {
+
+        #region FFM_JOB Master
+        List<FFM_JOB> GetFFM_JOB(string CmpyCode);
+        FFM_JOB_VM SaveFM_JOBHEAD(FFM_JOB_VM FJ);
+        bool DeleteFFM_JOB(string FFM_JOB_CODE, string CmpyCode, string UserName);
+
+        #endregion
+
+
+
+    }
+}
