02f1bbbaaf9e6767a330645c83a505121b19c582
sharyu <ezwaretechano@gmail.com>
2019-05-19 14:26:42 +0400

    master update

--- EzBusiness_ViewModels/Models/FreightManagement/FFM_JOB_VM.cs
+++ EzBusiness_ViewModels/Models/FreightManagement/FFM_JOB_VM.cs
@@ -14,7 +14,8 @@ namespace EzBusiness_ViewModels.Models.FreightManagement
         public DateTime CREATED_ON { get; set; }
         public string UPDATED_BY { get; set; }
         public DateTime UPDATED_ON { get; set; }
-        public string CMPYCODE { get; set; }      
+        public string CMPYCODE { get; set; }
+        [DisplayName("CODE")]
         public string FFM_JOB_CODE { get; set; }
         public string NAME { get; set; }
 
